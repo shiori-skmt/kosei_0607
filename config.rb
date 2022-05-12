5b212cd22dc5afad97c526b3f78554f3da093cf3
rtsuchida <r.tsuchida@coosy.co.jp>
2022-05-12 18:01:44 +0900

    ヘッダーざっくりコーディング

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+Encoding.default_external = 'utf-8'
+cache = false
+output_style = :compressed
+line_comments = false
+sourcemap = true
